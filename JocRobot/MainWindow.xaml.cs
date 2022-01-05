789602c783f425c690ee4ed64020162c47207882
Jogigo <a2006266@institutmontilivi.cat>
2022-01-05 13:33:44 +0100

    Versio millorada amb barra d'estat

--- JocRobot/MainWindow.xaml.cs
+++ JocRobot/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace JocRobot
 {
@@ -20,9 +21,34 @@ namespace JocRobot
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int NFILES = 3, NCOLUMNES = 3;
+        Tauler joc;
+
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
+            joc = new Tauler(NFILES, NCOLUMNES);
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
             InitializeComponent();
+            grdTaulell.Children.Add(joc);
+            grdTaulell.Background = Brushes.CadetBlue;
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            joc.posicioRobot();
+
+            if (joc.jocAcabat())
+            {
+                timer.Stop();
+                Window finestra = new wndFinal();
+                finestra.Show();
+
+            }
+        }
+
+
     }
 }
